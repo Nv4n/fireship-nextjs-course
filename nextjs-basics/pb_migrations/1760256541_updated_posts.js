fb715f6e1240cacff728137c9065eed5d4d05b03
Sybatron <vipsister2014@gmail.com>
2025-11-02 22:34:50 +0200

    add initial files

---
+++ nextjs-basics/pb_migrations/1760256541_updated_posts.js
@@ -0,0 +1,47 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_1125843985")
+
+  // update collection data
+  unmarshal({
+    "createRule": "",
+    "deleteRule": "",
+    "listRule": "",
+    "updateRule": "",
+    "viewRule": ""
+  }, collection)
+
+  // add field
+  collection.fields.addAt(2, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text4274335913",
+    "max": 0,
+    "min": 0,
+    "name": "content",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_1125843985")
+
+  // update collection data
+  unmarshal({
+    "createRule": null,
+    "deleteRule": null,
+    "listRule": null,
+    "updateRule": null,
+    "viewRule": null
+  }, collection)
+
+  // remove field
+  collection.fields.removeById("text4274335913")
+
+  return app.save(collection)
+})
